e4d94957801aa5e5fd047458ec4d8e1f062b4c89
zachyyy700 <zachyyy700@gmail.com>
2025-08-15 13:50:13 -0700

    Initial commit

---
+++ R/curry_amount_except_pentuple_it.R
@@ -0,0 +1,22 @@
+# new example
+#' Curry Amount for Curry-Loving Customer
+#'
+#' @param initial_amount A number representing the amount of servings of curry the customer wants. MUST be <= 2.
+#' @param customer_name A string representing that is the customer's name
+#'
+#' @returns A number that is five times more curry than they asked for. More curry is always good.
+#' @export
+#'
+#' @examples
+#' #curry_amount_except_pentuple_it(10, "Zach")
+curry_amount_except_pentuple_it <- function(initial_amount, customer_name) {
+  if (!is.character(customer_name)) {
+    stop("Please type letters.")
+  }
+  if (initial_amount <= 2) {
+    warning("You only want ", initial_amount, " servings of curry?? Please enter higher amount, you want more.")
+  }
+  new_curry_amount <- initial_amount * 5
+  paste(customer_name, "would like", new_curry_amount, "servings of curry.")
+}
+
---
+++ R/karee_nya.R
@@ -0,0 +1,5 @@
+# create function for currycat package
+# file name must be same as function name
+karee_nya <- function() {
+  print("One Curry Rice, kudasai")
+}
