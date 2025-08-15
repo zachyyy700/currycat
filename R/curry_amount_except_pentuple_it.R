25a50adac5aaa76418264d61bb5e032d4332d813
zachyyy700 <zachyyy700@gmail.com>
2025-08-15 13:52:54 -0700

    curry function

--- R/curry_amount_except_pentuple_it.R
+++ R/curry_amount_except_pentuple_it.R
@@ -1,8 +1,8 @@
 # new example
 #' Curry Amount for Curry-Loving Customer
 #'
-#' @param initial_amount A number representing the amount of servings of curry the customer wants. MUST be <= 2.
-#' @param customer_name A string representing that is the customer's name
+#' @param initial_amount A number representing the amount of curry servings the customer wants. MUST be <= 2.
+#' @param customer_name A string representing the customer's name
 #'
 #' @returns A number that is five times more curry than they asked for. More curry is always good.
 #' @export
@@ -14,7 +14,7 @@ curry_amount_except_pentuple_it <- function(initial_amount, customer_name) {
     stop("Please type letters.")
   }
   if (initial_amount <= 2) {
-    warning("You only want ", initial_amount, " servings of curry?? Please enter higher amount, you want more.")
+    stop("You only want ", initial_amount, " servings of curry?? Please enter higher amount, you want more.")
   }
   new_curry_amount <- initial_amount * 5
   paste(customer_name, "would like", new_curry_amount, "servings of curry.")
